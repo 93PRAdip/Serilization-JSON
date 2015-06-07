8270e8f18a28f77603877754aefa0ef5bf39f2e4
Pradip <pradip@Pradips-Mac.local>
2015-06-07 19:26:06 +0530

    Initial Commit

---
+++ Serilization JSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Serilization JSON
+//
+//  Created by Pradip on 6/7/15.
+//  Copyright (c) 2015 Pradip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
